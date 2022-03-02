350c64f1575e2470a20bfce239baaafc99ec9a1e
Anoop-LogCat <anoopbenzier1997@outlook.com>
2022-03-02 21:29:22 +0530

    :zap: perf(lib): configure codegen and add test graphql file

---
+++ apps/frontend/main/graphql/test.generated.ts
@@ -0,0 +1,16 @@
+import * as Types from '@workspace-starter/codegen-sdk/base-types';
+
+import { DocumentNode } from 'graphql';
+import * as Urql from 'urql';
+export type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
+export type GetUsersFrontendQueryVariables = Types.Exact<{ [key: string]: never; }>;
+
+
+export type GetUsersFrontendQuery = { __typename?: 'query_root', users: Array<{ __typename?: 'users', id: any, display_name?: string | null, account?: { __typename?: 'auth_accounts', email?: any | null } | null }> };
+
+
+export const GetUsersFrontendDocument = {"kind":"Document","definitions":[{"kind":"OperationDefinition","operation":"query","name":{"kind":"Name","value":"getUsersFrontend"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"users"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"id"}},{"kind":"Field","name":{"kind":"Name","value":"display_name"}},{"kind":"Field","name":{"kind":"Name","value":"account"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"email"}}]}}]}}]}}]} as unknown as DocumentNode;
+
+export function useGetUsersFrontendQuery(options?: Omit<Urql.UseQueryArgs<GetUsersFrontendQueryVariables>, 'query'>) {
+  return Urql.useQuery<GetUsersFrontendQuery>({ query: GetUsersFrontendDocument, ...options });
+};
