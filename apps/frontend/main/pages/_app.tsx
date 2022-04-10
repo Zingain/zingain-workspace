242662ee63016723c14fed9b63a1dfd4ae40a755
Anoop-LogCat <anoopbenzier1997@outlook.com>
2022-04-10 12:21:04 +0530

    :recycle: refactor(frontend): add a welcome page for initial setup

--- apps/frontend/main/pages/_app.tsx
+++ apps/frontend/main/pages/_app.tsx
@@ -1,24 +1,16 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
-import { NhostApolloProvider } from '@nhost/react-apollo';
-import { NhostAuthProvider } from '@nhost/react-auth'
 import '../styles/global.css';
-
-import { client } from '../helpers/nhost';
 
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <NhostAuthProvider nhost={client}>
-        <NhostApolloProvider graphqlUrl={process.env.NEXT_PUBLIC_GRAPHQL_URL }>
-          <Head>
-            <title>Zingain Workspace</title>
-          </Head>
-          <main className="app">
-            <Component {...pageProps} />
-          </main>
-        </NhostApolloProvider>
-      </NhostAuthProvider>
+      <Head>
+        <title>Welcome to main!</title>
+      </Head>
+      <main className="app">
+        <Component {...pageProps} />
+      </main>
     </>
   );
 }
--- apps/frontend/main/pages/index.tsx
+++ apps/frontend/main/pages/index.tsx
@@ -1,8 +1,18 @@
-import styles from './index.module.css';
-
 export function Index() {
   return (
-    <div><h1>Home Page</h1></div>
+    <section className="relative">
+
+      <div className="max-w-6xl mx-auto px-4 sm:px-6">
+        <div className="pt-32 pb-12 md:pt-40 md:pb-20">
+          <div className="text-center pb-12 md:pb-16">
+            <h1 className="text-5xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4" data-aos="zoom-y-out">Zingain <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400">workspace</span></h1>
+            <div className="max-w-3xl mx-auto">
+              <p className="text-xl text-gray-600 mb-8" data-aos="zoom-y-out" data-aos-delay="150">A initial workspace setup of Zingain solutions developed using NX, Hasura, Nest, and Next js. The other technologies using were tailwindcss, codegen, hasura backend plus.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
 
